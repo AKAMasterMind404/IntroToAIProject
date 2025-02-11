051d1c9429923df58220a5de17649e0cc2893f92
Atharv Karbhari <59548859+AKAMasterMind404@users.noreply.github.com>
2025-02-11 17:31:47 -0500

    Pygame integrated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import graph.graph as g
 
 if __name__ == "__main__":
-    n = 60
-    G = g.create_manhattan_graph(n)
-    g.visualize_manhattan_graph(G, n)
+    n = 30 # Grid size
+    g.start_gui(n)
